b87f2d5d758b1878d0a82d3d07352467f30ab3f3
Sezer Gümüş <66944231+Sezergumus@users.noreply.github.com>
2024-08-31 14:46:00 +0300

    Update About.jsx

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -19,7 +19,7 @@ export default function About() {
         </div>
         <div className="about-content">
             <div className="about-text">
-              <Reveal><p className="mb-[32px] text-[24px]">Hello, I&apos;m Sezer and I&apos;m a 19-year-old computer science student with a passion for web development.</p></Reveal>
+              <Reveal><p className="mb-[32px] text-[24px]">Hello, I&apos;m Sezer and I&apos;m a 20-year-old computer science student with a passion for web development.</p></Reveal>
               <Reveal><p className="mb-[32px] text-[24px]">Since I was a kid, I have been fascinated by computers. <br/>At the age of 10, I started programming with Scratch and Code.org.</p></Reveal>
               <Reveal><p className="mb-[32px] text-[24px]">I continued my journey by learning Python. As I delved deeper into the world of programming, I discovered C# and used it to develop games with Unity. </p></Reveal>
               <Reveal><p className="mb-[32px] text-[24px]">However, it was web development that truly captured my interest. As a self-taught full-stack developer, I have spent the past 3 years honing my skills in the MERN stack.</p></Reveal>
